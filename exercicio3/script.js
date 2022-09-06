2885cd117e60757fcf507731a96b4acd6ccff62a
alinekabbas <alinekabbas@gmail.com>
2022-09-06 16:10:23 -0300

    Exercicios corrigidoa

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,9 +1,9 @@
 //Crie um código que receba um **prompt** que diga: 
 //"Escreva aqui sua nacionalidade" e guarde o valor da resposta em uma const `nacionalidade`.
 
-let nacionalidadePergunta = prompt("Escreva aqui sua nacionalidade")
-const resposta = nacionalidadePergunta
-const nacionalidades = ['brasileira','argentina', 'uruguaia', 'chilena', 'colombiana']
+let nacionalidade = prompt("Escreva aqui sua nacionalidade").toLowerCase()
+//console.log(respostaNacionalidade)
+const nacionalidadeResposta = ['brasileira','argentina', 'uruguaia', 'chilena', 'colombiana']
 
 // Vamos considerar que nosso programa aceita as seguintes 5 nacionalidades:
 // - brasileira;
@@ -16,8 +16,8 @@ const nacionalidades = ['brasileira','argentina', 'uruguaia', 'chilena', 'colomb
 //e caso seja, imprima a nacionalidade no console. 
 //O último `else` deve imprimir "nacionalidade não encontrada" caso o valor de `nacionalidade` não corresponda a nenhum dos valores.
 
-if(nacionalidadePergunta === nacionalidades){
-    console.log(resposta)
-}else{
-    console.log('nacionalidade não encontrada')
+if(nacionalidadeResposta.includes(nacionalidade)){
+     console.log(nacionalidade)
+}else {
+     console.log('Nacionalidade não encontrada')
 }
